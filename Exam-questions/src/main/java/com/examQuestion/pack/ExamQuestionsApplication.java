d98d95ef271602c0f09c5faee75407e46c4a825a
shubham Kumar Nimje <shubbham13@gmail.com>
2024-01-30 18:31:07 +0530

    initial

---
+++ Exam-questions/src/main/java/com/examQuestion/pack/ExamQuestionsApplication.java
@@ -0,0 +1,13 @@
+package com.examQuestion.pack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamQuestionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamQuestionsApplication.class, args);
+	}
+
+}
---
+++ Exam-questions/src/test/java/com/examQuestion/pack/ExamQuestionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examQuestion.pack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamQuestionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
